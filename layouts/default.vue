3243c3e8312ac4e12880436f8ddd0178eeb0a728
Sveinn Bjarki Brynjarsson <sveinnbb@gmail.com>
2020-06-30 15:23:15 +0000

    yellow

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
   <v-app>
     <Navbar />
     <v-main>
-      <v-container :style="'max-width: ' + maxWidth" class="pink">
+      <v-container :style="'max-width: ' + maxWidth" class="yellow">
         <nuxt class="content" />
       </v-container>
     </v-main>
