317a5e99423c9dde0bf9a1b0398c527238e91150
Sveinn Bjarki Brynjarsson <sveinnbb@gmail.com>
2020-07-22 15:34:01 +0000

    reviews card responsive font

--- components/slices/Reviews.vue
+++ components/slices/Reviews.vue
@@ -2,11 +2,11 @@
   <v-container>
     <v-row justify="center">
       <v-card v-for="(item, index) in slice.items" :key="'review_' + index" max-width="800" class="ma-2">
-        <v-card-text class="text-subtitle-1">
+        <v-card-text class="text-sm-subtitle-1">
           <prismic-rich-text :field="item.text"/>
         </v-card-text>
-        <v-card-title class="text-h5">{{ item.name }}</v-card-title>
-        <v-card-subtitle class="text-subtitle-1">{{ item.role }}</v-card-subtitle>
+        <v-card-title class="text-sm-h5">{{ item.name }}</v-card-title>
+        <v-card-subtitle class="text-sm-subtitle-1">{{ item.role }}</v-card-subtitle>
       </v-card>
     </v-row>
   </v-container>
